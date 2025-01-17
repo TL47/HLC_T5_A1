a8c4d79ef196e3b2649cb8403c76b3ee63b9f5cd
TL47 <tlopdel0611@g.educaand.es>
2025-01-17 18:56:56 +0100

    add 7,8

---
+++ ejercicio7.py
@@ -0,0 +1,10 @@
+def suma(n1, n2):
+    numero = n1 + n2
+    print("La suma de los dos números es:", numero)
+
+def segundo(n1):
+    n2=int(input("Introduzca el segundo número:"))
+    suma(n1,n2)
+
+n1=int(input("Introduzca el primer número:"))
+segundo(n1)
---
+++ ejercicio8.py
@@ -0,0 +1,6 @@
+numero_base=int(input("Introduzca un numero base:"))
+numero_altura_triangulo=int(input("Introduzca la altura del triaungulo:"))
+numero_precio=int(input("Introduzca el precio por metro cuadrado:"))
+area_triangulo=numero_base*numero_altura_triaungulo/2
+total=area_triangulo*numero_precio
+print("Area =", area_triangulo,"metros cuadrados.", "Costo total", total, "unidades.")
